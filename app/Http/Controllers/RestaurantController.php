f20839823c4215718a3f6fe12508b71b8a4f7b96
ayanakkabi1 <aynour200609@gmail.com>
2026-02-03 14:44:16 +0100

    YD-9 fix: enhance RestaurantController with create and store methods

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,11 +2,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
-use Illuminate\Http\Request;  
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class RestaurantController extends Controller
 {
     public function index(){
         $restaurants=Restaurant::all();
         return view('restaurant.index',compact('restaurants'));
     }  
+    public function create(){
+        return view('restaurant.create');
+    }
+    public function store(Request $request){
+        if(Auth::user()->role != 'restaurateur'){
+            return back()->with('error','Accès refusé');
+        }
+        Restaurant::create($request->validated());
+    }
 }
