1ba5a1d45c3be5c6d963a4181cafb269e5342101
ayanakkabi1 <aynour200609@gmail.com>
2026-02-03 13:55:47 +0100

    YD-9 feat: Implement RestaurantController with index method to list all restaurants

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Restaurant;
+use Illuminate\Http\Request;  
+class RestaurantController extends Controller
+{
+    public function index(){
+        $restaurants=Restaurant::all();
+        return view('restaurant.index',compact('restaurants'));
+    }  
+}
