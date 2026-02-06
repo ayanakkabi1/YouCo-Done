c5aa8f375dcceee820b5342add166d4bd950dd8e
ayanakkabi1 <aynour200609@gmail.com>
2026-02-06 12:11:37 +0100

    YD-20 fix: correct route casing and reorder restaurant routes for clarity

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-route::get('/restaurants',[RestaurantController::class,'index'])->name('restaurant.index');
+
 route::get('/restaurants/create',[RestaurantController::class,'create'])->name('restaurant.create');
 route::post('/restaurant/store',[RestaurantController::class,'store'])->name('restaurant.store');
 route::get('restaurant/{id}/edit',[RestaurantController::class,'edit'])->name('restaurant.edit');
-route::PUT('restaurant/{id}/update',[RestaurantController::class,'update'])->name('restaurant.update');
+route::put('restaurant/{id}/update',[RestaurantController::class,'update'])->name('restaurant.update');
+
+route::get('/restaurants',[RestaurantController::class,'index'])->name('restaurant.index');
+route::get('/restaurant/{id}',[RestaurantController::class,'deatils'])->name('restaurant.details');
+Route::get('/restaurants/search', [RestaurantController::class, 'search'])->name('restaurant.search');
 require __DIR__.'/auth.php';
